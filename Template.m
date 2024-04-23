f82242b62981764083c04c574e86eb0fda6529e3
olliefrancis84 <egyof1@nottingham.ac.uk>
2024-04-23 14:31:25 +0100

    Task 1 complete

--- Template.m
+++ Template.m
@@ -3,7 +3,6 @@
 %% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [10 MARKS]
 clear
 a = arduino("COM3")
-n = 10
 for f = 1:10
     writeDigitalPin(a,'D10',1)
     pause(0.5)
@@ -11,11 +10,42 @@ for f = 1:10
     pause(0.5)
 end
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
-
-% Insert answers here
-
+fileID = fopen('cabin_temperature.txt','w');
+time = 1:1:601;
+Voltage = zeros(1,601);
+temperature = zeros(1,601);
+for f = 1:601
+    Voltage(f) = readVoltage(a,'A4');
+ 
+    temperature(f) = ((Voltage(f)-0.5)*100);
+end
+Minimum = min(temperature) 
+Maximum = max(temperature)
+Average = mean(temperature)
+plot(time,temperature)
+xlabel('time/S')
+ylabel('temperature/C')
+A1 = datetime('now','TimeZone','local','Format','dd/MM/uuuu');
+disp(fprintf(fileID,'Data logging initiated-%s\nLocation-Nottingham',A1))
+n = 1;
+D = 0
+for i = 0:10
+    A3 = D;
+    disp(fprintf(fileID,'\nMinute    \t\t %d',A3))
+    A2 = temperature(n);
+    disp(fprintf(fileID,'\nTemperature\t\t %2.2f',A2))
+    n = n+60;
+    D = D+1;
+end
+A4 = Maximum;
+A5 = Minimum;
+A6 = Average;
+disp(fprintf(fileID,'\nMax temp  \t\t%2.2fC',A4))
+disp(fprintf(fileID,'\nMin temp  \t\t%2.2fC',A5))
+disp(fprintf(fileID,'\nAverage temp \t%2.2fC',A6))
+disp(fprintf(fileID,'\nData logging terminated'))
 %% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
-
+doc sprintf
 % Insert answers here
 
 
