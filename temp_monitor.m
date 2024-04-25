7b1ed6682383bb77a3df3c979e91a047ee32f8c2
olliefrancis84 <egyof1@nottingham.ac.uk>
2024-04-25 12:00:45 +0100

    Task 2

---
+++ temp_monitor.m
@@ -0,0 +1,40 @@
+clear
+a = arduino("COM3");
+temperature = ((readVoltage(a,'A4')-0.5)*100);
+time = 0;
+figure
+while (temperature>0) && (temperature<100)
+    if ((18 <= temperature) && (temperature <= 24))
+        writeDigitalPin(a,"D6",1)
+        pause(1)
+       
+    elseif (temperature < 18)
+        writeDigitalPin(a,"D6",0)
+        
+        writeDigitalPin(a,"D4",1)
+        pause(0.5)
+        writeDigitalPin(a,"D4",0)
+        pause(0.5)
+    else 
+        writeDigitalPin(a,"D6",0)
+        writeDigitalPin(a,"D2",1)
+        pause(0.25)
+        writeDigitalPin(a,"D2",0)
+        pause(0.25)  
+        writeDigitalPin(a,"D2",1)
+        pause(0.25)
+        writeDigitalPin(a,"D2",0)
+        pause(0.25)
+        
+    end
+    temperature = ((readVoltage(a,'A4')-0.5)*100)
+    time = time +1;
+    xlabel("time/s")
+    ylabel("temperature/C")
+    ylim([10 28])
+    xlim([0 100])
+    plot(time,temperature,'.-')
+    hold on
+    
+end
+writeDigitalPin(a,"D6",0)
