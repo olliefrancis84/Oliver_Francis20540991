b6ca7286dc38050cdbcc721c3beaefbb181d48dd
olliefrancis84 <egyof1@nottingham.ac.uk>
2024-04-25 11:51:36 +0100

    Task 2

--- Template.m
+++ Template.m
@@ -2,7 +2,6 @@
 %egyof1@nottingham.ac.uk
 %% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [10 MARKS]
 clear
-a = arduino("COM3")
 for f = 1:10
     writeDigitalPin(a,'D10',1)
     pause(0.5)
@@ -45,8 +44,48 @@ disp(fprintf(fileID,'\nMin temp  \t\t%2.2fC',A5))
 disp(fprintf(fileID,'\nAverage temp \t%2.2fC',A6))
 disp(fprintf(fileID,'\n\nData logging terminated'))
 %% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
-doc sprintf
-% Insert answers here
+clear
+a = arduino("COM3");
+temperature = ((readVoltage(a,'A4')-0.5)*100);
+time = 0;
+figure
+while (temperature>0) && (temperature<100)
+    if ((18 <= temperature) && (temperature <= 24))
+        writeDigitalPin(a,"D6",1)
+        pause(1)
+       
+    elseif (temperature < 18)
+        writeDigitalPin(a,"D6",0)
+        
+        writeDigitalPin(a,"D4",1)
+        pause(0.5)
+        writeDigitalPin(a,"D4",0)
+        pause(0.5)
+    else 
+        writeDigitalPin(a,"D6",0)
+        writeDigitalPin(a,"D2",1)
+        pause(0.25)
+        writeDigitalPin(a,"D2",0)
+        pause(0.25)  
+        writeDigitalPin(a,"D2",1)
+        pause(0.25)
+        writeDigitalPin(a,"D2",0)
+        pause(0.25)
+        
+    end
+    temperature = ((readVoltage(a,'A4')-0.5)*100)
+    time = time +1;
+    xlabel("time/s")
+    ylabel("temperature/C")
+    ylim([10 28])
+    xlim([0 100])
+    plot(time,temperature,'.-')
+    hold on
+    
+end
+writeDigitalPin(a,"D6",0)
+
+
 
 
 %% TASK 3 - ALGORITHMS – TEMPERATURE PREDICTION [25 MARKS]
